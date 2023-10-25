9218ab9e9457f36ce9fe2ceda9f1edac383abb85
William Feng <wfeng@clickup.com>
2023-10-25 13:44:47 -0400

    add optional extensions for subscription data and next

--- packages/graphql/lib/src/links/websocket_link/websocket_messages.dart
+++ packages/graphql/lib/src/links/websocket_link/websocket_messages.dart
@@ -87,9 +87,11 @@ abstract class GraphQLSocketMessage extends JsonSerializable {
         return PongMessage(payload as Map<String, dynamic>);
 
       case MessageTypes.data:
-        return SubscriptionData(id, payload['data'], payload['errors']);
+        return SubscriptionData(id, payload['data'], payload['errors'],
+            extensions: payload['extensions']);
       case MessageTypes.next:
-        return SubscriptionNext(id, payload['data'], payload['errors']);
+        return SubscriptionNext(id, payload['data'], payload['errors'],
+            extensions: payload['extensions']);
       case MessageTypes.error:
         return SubscriptionError(id, payload);
       case MessageTypes.complete:
@@ -240,17 +242,20 @@ class ConnectionKeepAlive extends GraphQLSocketMessage {
 /// payload. The user should check the errors result before processing the
 /// data value. These error are from the query resolvers.
 class SubscriptionData extends GraphQLSocketMessage {
-  SubscriptionData(this.id, this.data, this.errors) : super(MessageTypes.data);
+  SubscriptionData(this.id, this.data, this.errors, {this.extensions})
+      : super(MessageTypes.data);
 
   final String id;
   final dynamic data;
   final dynamic errors;
+  final dynamic extensions;
 
   @override
   toJson() => {
         "type": type,
         "data": data,
         "errors": errors,
+        "extensions": extensions,
       };
 
   @override
@@ -262,18 +267,17 @@ class SubscriptionData extends GraphQLSocketMessage {
 }
 
 class SubscriptionNext extends GraphQLSocketMessage {
-  SubscriptionNext(this.id, this.data, this.errors) : super(MessageTypes.next);
+  SubscriptionNext(this.id, this.data, this.errors, {this.extensions})
+      : super(MessageTypes.next);
 
   final String id;
   final dynamic data;
   final dynamic errors;
+  final dynamic extensions;
 
   @override
-  toJson() => {
-        "type": type,
-        "data": data,
-        "errors": errors,
-      };
+  toJson() =>
+      {"type": type, "data": data, "errors": errors, "extensions": extensions};
 
   @override
   int get hashCode => toJson().hashCode;
